5b30bcd639244ab0f31bd9c293bf10424fee0305
Filip <filipwedzicha@gmail.com>
2023-09-26 13:02:13 +0100

    Fix python2to3 related issues + reformat all code

--- swiffas/__init__.py
+++ swiffas/__init__.py
@@ -1,6 +1,8 @@
 from . import swfparse
 from . import avm2
+from . import deserialise
 
 # make top level parsers also module top level
 from .swfparse import SWFParser
 from .avm2 import ABCFile
+from .deserialise import Unpackable
--- swiffas/swfparse.py
+++ swiffas/swfparse.py
@@ -1,127 +1,128 @@
-import io
-import zlib
 # import pylzma - not a hard dependency; see loader below
+import zlib
+from struct import unpack_from, calcsize
 
-from .deserialise import Unpackable, BitObject
-from struct import unpack, unpack_from, calcsize
-
+from .deserialise import Unpackable
 from .swftags import TAG_HANDLERS, SWFRectangle
-from io import StringIO
 
-class SWFHeader (Unpackable):
-	_struct = [
-		('c', 'sig0'),
-		('c', 'sig1'),
-		('c', 'sig2'),
-		('B', 'version'),
-		('I', 'file_length')]
 
-	_compression_signatures = {
-		'F': None,
-		'C': 'zlib',
-		'Z': 'lzma'
-	}
+class SWFHeader(Unpackable):
+    _struct = [
+        ('c', 'sig0'),
+        ('c', 'sig1'),
+        ('c', 'sig2'),
+        ('B', 'version'),
+        ('I', 'file_length')]
 
-	@property
-	def compression (self):
-		return self._compression_signatures[self.sig0]
+    _compression_signatures = {
+        b'F': None,
+        b'C': 'zlib',
+        b'Z': 'lzma'
+    }
 
-class SWFRecordHeader (Unpackable):
-	def __init__ (self, data, offset):
-		tagcode_and_length = unpack_from ('<H', data, offset)[0]
-		self.tagcode = tagcode_and_length >> 6
-		self.length = tagcode_and_length & ((1 << 6) - 1)
+    @property
+    def compression(self):
+        return self._compression_signatures[self.sig0]
 
-		self._size = calcsize ('<H')
-		offset += calcsize ('<H')
 
-		if self.length >= 0x3F:
-			self.length = unpack_from ('<I', data, offset)[0]
-			self._size += calcsize ('<I')
+class SWFRecordHeader(Unpackable):
+    def __init__(self, data, offset):
+        tagcode_and_length = unpack_from('<H', data, offset)[0]
+        self.tagcode = tagcode_and_length >> 6
+        self.length = tagcode_and_length & ((1 << 6) - 1)
 
-class SWFProperties (Unpackable):
-	_struct = [
-		(SWFRectangle, 'frame_size'),
-		('H', 'frame_rate'),
-		('H', 'frame_count')]
+        self._size = calcsize('<H')
+        offset += calcsize('<H')
 
-class SWFParser (object):
-	def __init__ (self, file=None):
-		self.header = None
-		self.properties = None
+        if self.length >= 0x3F:
+            self.length = unpack_from('<I', data, offset)[0]
+            self._size += calcsize('<I')
 
-		# list of parsed tag objects, in file order
-		self.tags = []
 
-		# list of SWFRecordHeaders, in file order (parsed and unparsed tags)
-		self.record_headers = [] 	
+class SWFProperties(Unpackable):
+    _struct = [
+        (SWFRectangle, 'frame_size'),
+        ('H', 'frame_rate'),
+        ('H', 'frame_count')]
 
-		if file:
-			self.parse (file)
 
-	def parse (self, filelike, warn_unknown_tags=True):
-		# grab the header
-		offset = 0
+class SWFParser(object):
+    def __init__(self, file=None):
+        self.header = None
+        self.properties = None
 
-		# header is 8 bytes long
-		hsize = SWFHeader._ssize()
-		self.header = SWFHeader (filelike.read (hsize), offset, hsize)
-		offset += self.header._size
+        # list of parsed tag objects, in file order
+        self.tags = []
 
-		if self.header.compression == 'zlib':
-			# for now, just decompress the whole thing
-			decompressed = zlib.decompress (filelike.read())
-			fbuf = decompressed
-			# filelike = StringIO (decompressed)
+        # list of SWFRecordHeaders, in file order (parsed and unparsed tags)
+        self.record_headers = []
 
-			# ensure decompressed size matches file size
-			assert self.header.file_length - hsize == len(decompressed)
+        if file:
+            self.parse(file)
 
-			# reset offset since we will load from compressed buffer
-			# which is already an offset of 8 bytes in
-			offset = 0
-		elif self.header.compression == 'pylzma':
-			import pylzma
-			decompressed = pylzma.decompress (filelike.read())
-			fbuf = decompressed
-			assert self.header.file_length - hsize == len(decompressed)
-			offset = 0
-		else:
-			fbuf = filelike.read()
-			offset = 0
+    def parse(self, filelike, warn_unknown_tags=True):
+        # grab the header
+        offset = 0
 
-		self.properties = SWFProperties (fbuf, offset)
-		offset += self.properties._size
+        # header is 8 bytes long
+        hsize = SWFHeader._ssize()
+        self.header = SWFHeader(filelike.read(hsize), offset, hsize)
+        offset += self.header._size
 
-		# now read all the headers we have
-		self.tags = []
-		self.record_headers = []
+        if self.header.compression == 'zlib':
+            # for now, just decompress the whole thing
+            decompressed = zlib.decompress(filelike.read())
+            fbuf = decompressed
+            # filelike = StringIO (decompressed)
 
-		file_offset = (self.properties._size + self.header._size)
-		hdr_count = 0
-		while file_offset < self.header.file_length:
-			hdr = SWFRecordHeader (fbuf, offset)
-			self.record_headers.append (hdr)
+            # ensure decompressed size matches file size
+            assert self.header.file_length - hsize == len(decompressed)
 
-			offset += hdr._size
-			file_offset += hdr._size
-			hdr_count += 1
+            # reset offset since we will load from compressed buffer
+            # which is already an offset of 8 bytes in
+            offset = 0
+        elif self.header.compression == 'pylzma':
+            import pylzma
+            decompressed = pylzma.decompress(filelike.read())
+            fbuf = decompressed
+            assert self.header.file_length - hsize == len(decompressed)
+            offset = 0
+        else:
+            fbuf = filelike.read()
+            offset = 0
 
-			if hdr.tagcode in TAG_HANDLERS:
-				handler = TAG_HANDLERS[hdr.tagcode]
-				tag = handler(fbuf, offset, remaining=hdr.length)
+        self.properties = SWFProperties(fbuf, offset)
+        offset += self.properties._size
 
-				# make sure we deserialised the whole tag
-				assert tag._size == hdr.length, \
-					"only deserialised %d of %d bytes in tag %r" % (
-						tag._size,
-						hdr.length,
-						tag
-					)
+        # now read all the headers we have
+        self.tags = []
+        self.record_headers = []
 
-				self.tags.append (tag)
-			elif warn_unknown_tags:
-				print('warning: unknown tag', hdr.tagcode)
+        file_offset = (self.properties._size + self.header._size)
+        hdr_count = 0
+        while file_offset < self.header.file_length:
+            hdr = SWFRecordHeader(fbuf, offset)
+            self.record_headers.append(hdr)
 
-			offset += hdr.length
-			file_offset += hdr.length
+            offset += hdr._size
+            file_offset += hdr._size
+            hdr_count += 1
+
+            if hdr.tagcode in TAG_HANDLERS:
+                handler = TAG_HANDLERS[hdr.tagcode]
+                tag = handler(fbuf, offset, remaining=hdr.length)
+
+                # make sure we deserialised the whole tag
+                assert tag._size == hdr.length, \
+                    "only deserialised %d of %d bytes in tag %r" % (
+                        tag._size,
+                        hdr.length,
+                        tag
+                    )
+
+                self.tags.append(tag)
+            elif warn_unknown_tags:
+                print('warning: unknown tag', hdr.tagcode)
+
+            offset += hdr.length
+            file_offset += hdr.length
