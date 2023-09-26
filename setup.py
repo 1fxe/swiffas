de7c546e418b4f79a18d9c5a177dc29597de4c86
Filip <filipwedzicha@gmail.com>
2023-09-26 12:37:15 +0100

    Change setup.py url to this

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='swiffas',
       version='0.1',
       description='SWF parser and AVM2 (Actionscript 3) bytecode parser',
-      url='http://github.com/ahixon/swiffas',
+      url='https://github.com/1fxe/swiffas',
       author='Alex Hixon',
       author_email='alex@alexhixon.com',
       license='MIT',
